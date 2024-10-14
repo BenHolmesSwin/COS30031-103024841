76ebbe183079df177a322a6fc6c64c377455e62a
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-14 16:19:03 +1100

    update and exit done, game complete!

--- 05c - Spike - Gridworld/GridWorld/GridWorld.cpp
+++ 05c - Spike - Gridworld/GridWorld/GridWorld.cpp
@@ -17,6 +17,7 @@ using namespace std;
 // S is start
 // ' ' is empty
 // G is gold
+// REMINDER FOR BEN: first is y, second is x
 char map[8][8]{
 	{'#','#','#','#','#','#','#','#'},
 	{'#',' ',' ',' ',' ',' ','G','#'},
@@ -42,12 +43,18 @@ bool playing = true;
 // bool for goal or death end.
 bool sucess = false;
 
+// bool for quit
+bool quit = false;
+
 // current options as list of char
 list<char> options;
 
+
 void display() {
 	cout << "===Current Map===" << endl;
+	//looping y axis
 	for (int i = 0; i <= 7; i++) {
+		//looping x axis
 		for (int j = 0; j <= 7; j++) {
 			//spacing
 			cout << ' ';
@@ -68,7 +75,7 @@ void display() {
 
 char input() {
 	bool badInput = true;
-	char input;
+	char option;
 	while (badInput) {
 		cout << "Exit program is Q" << endl;
 		cout << "You can move ";
@@ -76,15 +83,15 @@ char input() {
 			cout << option << ' ';
 		}
 		cout << ":> ";
-		cin >> input;
+		cin >> option;
 		//check all options against input
-		for (char option : options) {
-			if (input == option) {
+		for (char posibleOption : options) {
+			if (posibleOption == option) {
 				badInput = false;//ends loop when false
 			}
 		}
 		//check input for Q for quit
-		if (input == 'Q') {
+		if (option == 'Q') {
 			badInput = false;
 		}
 		//if bad input, state it
@@ -92,28 +99,89 @@ char input() {
 			cout << "Bad Input. Try Again:" << endl;
 		}
 	}
-	return input;
+	return option;
 }
 
 void update(char option) {
-	
+	//switch for input option
+	//doesnt have check for invalid changes as input validation comparing to options is enough
+	switch (option)
+	{
+		case 'N':
+			playerY -= 1;
+			break;
+		case 'E':
+			playerX += 1;
+			break;
+		case 'S':
+			playerY += 1;
+			break;
+		case 'W':
+			playerX -= 1;
+			break;
+		case 'Q':
+			playing = false;
+			quit = true; //changing quit to true for end check
+			break;
+		default://for inital creation so options can be created without input
+			break;
+	}
+
+	//check for end
+	if (map[playerY][playerX] == 'D' || map[playerY][playerX] == 'G') {
+		playing = false;
+		if (map[playerY][playerX] == 'G') {
+			sucess = true;
+		}
+	}
+	else {
+		//making new options
+		options.clear();
+		
+		if (playerY > 0) {
+			if (map[playerY - 1][playerX] != '#') {
+				options.push_back('N');
+			}
+		}
+		if (playerX < 7) {
+			if (map[playerY][playerX + 1] != '#') {
+				options.push_back('E');
+			}
+		}
+		if (playerY < 7) {
+			if (map[playerY + 1][playerX] != '#') {
+				options.push_back('S');
+			}
+		}
+		if (playerX > 0) {
+			if (map[playerY][playerX - 1] != '#') {
+				options.push_back('W');
+			}
+		}
+	}
 }
 
 int main() {
 	//inital values
-	options.push_back('N');
-	options.push_back('E');
-	options.push_back('S');
-	options.push_back('W');
+	update(' ');//inital options creation
 
 	while (playing) {
 		display();
 		char option = input();
 		update(option);
-		playing = false;//exit loop on first for debug
 	}
-
-
-
+	if (quit) {// check for quit
+		cout << "You have exited the game. Thanks for playing!" << endl;
+	}
+	else if (sucess) {// check for gold reached
+		cout << "Wow - you’ve discovered a large chest filled with GOLD coins!" << endl;
+		cout << "YOU WIN!" << endl;
+		cout << "Thanks for playing. There probably won’t be a next time." << endl;
+	}
+	else {
+		cout << "Arrrrgh... you have fallen down a pit and landed on spikes." << endl;
+		cout << "YOU HAVE DIED!" << endl;
+		cout << "Thanks for playing. There probably won’t be a next time." << endl;
+	}
 	return 0;
 }

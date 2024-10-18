52e942893582e18ca22b65a5c7401290a0958fb2
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-18 19:15:50 +1100

    created testing functionality and added view inventory, on to report

--- 10c - Spike - Game Data Structures/Spike10/Spike10.cpp
+++ 10c - Spike - Game Data Structures/Spike10/Spike10.cpp
@@ -32,15 +32,32 @@ public:
     }
     vector<Item> inventory;
     int health;
-    void addItem(Item item) {
+    void addItem(Item item) {//adds item
         inventory.push_back(item);
     }
-    void removeItem(int id) {
-        inventory.erase(inventory.begin() + id);
+    void removeItem(int index) {//removes item via index
+        inventory.erase(inventory.begin() + index);
+    }
+    void showInventory() {//prints entire inventory
+        cout << "==Inventory==" << endl;
+        for (int i = 0; i < inventory.size(); i++) {
+            cout << "Inventory Id: " << i << " |";
+            cout << "Item id: " << inventory[i].id << " |";
+            cout << "Item type: " << inventory[i].type << " |";
+            cout << inventory[i].description << " |";
     }
 };
 
 int main()
 {
     //player creation and inventory
+    cout << "Adding a Rope" << endl;
+    player.addItem(Item(4, Rope, "A rope you can use to climb"));
+    player.showInventory();
+
+    //deleting item
+    cout << "deleteing a sword (item id 1)" << endl;
+    player.removeItem(1);//index
+    player.showInventory();
+
 }

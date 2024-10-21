efaf8fdab57106683e82aa4f87fcf662115dd51a
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-21 17:35:27 +1100

    command line parsing done, moved functions out of zorkish.cpp(main)

--- 11c - Spike - Game Graphs from Data/Zorkish/Adventure.cpp
+++ 11c - Spike - Game Graphs from Data/Zorkish/Adventure.cpp
@@ -31,11 +31,87 @@ Adventure::Adventure(const char* fileName)
 void Adventure::Start(json data){
 
 	data.at("locations").get_to(graph);
+	update("castle");
+}
 
-	//for testing output currently
+void Adventure::update(string s) {
 	for (location::Location l : graph) {
-		if (l.id == "castle") {
+		if (l.id == s) {
 			current = l;
 		}
 	}
 }
+
+vector<string> split(string& s, const string& delimiter) {// split tokens
+	vector<string> tokens;
+	size_t pos = 0;
+	string token;
+	while ((pos = s.find(delimiter)) != string::npos) {
+		token = s.substr(0, pos);
+		tokens.push_back(token);
+		s.erase(0, pos + delimiter.length());
+	}
+	tokens.push_back(s);
+
+	return tokens;
+}
+
+//For better zorkish, move game renderer
+//renders current location
+void Adventure::gameRender() {
+	cout << "You are currently at: " << current.name << endl;
+	cout << current.description << endl;
+
+}
+
+string Adventure::gameInput() {
+	bool badInput = true;
+	string input;
+	string result;
+	while (badInput) {
+		cout << "You may GO to: ";
+		for (string s : current.connections) {
+			cout << s << " | ";
+		}
+		cout << endl;
+		cout << ":> ";
+		getline(cin, input);
+		string delimiter = " ";
+		auto tokens = split(input, delimiter);
+		if (tokens.size() == 2) {
+			if (tokens[0] == "GO") {
+				//check all options against input
+				for (string posibleOption : current.connections) {
+					if (posibleOption == tokens[1]) {
+						result = posibleOption;
+						badInput = false;
+					}
+				}
+
+			}
+		}//check input for Q for quit
+		else if (tokens[0] == "QUIT") {
+			result = "QUIT";
+			badInput = false;
+		}
+		else {
+			cout << "Bad Input. Try Again:" << endl;
+		}
+	}
+	return result;
+}
+
+bool Adventure::gameUpdate(string input) {
+	if (input == "QUIT") {
+		return false;
+	}
+	for (location::Location l : graph) {
+		if (l.id == input) {
+			current = l;
+			return true;
+		}
+	}
+	return true;
+}
+
+
--- 11c - Spike - Game Graphs from Data/Zorkish/Adventure.h
+++ 11c - Spike - Game Graphs from Data/Zorkish/Adventure.h
@@ -8,10 +8,13 @@ using json = nlohmann::json;
 class Adventure
 {
 public:
+
 	Adventure(const char* fileName);
 	location::Location current;
 	void Start(json data);
 	vector<location::Location> graph;
-private:
-
+	void update(string s);
+	void gameRender();
+	string gameInput();
+	bool gameUpdate(string input);
 };

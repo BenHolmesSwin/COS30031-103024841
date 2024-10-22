de20e0fdcf31d1a7ebccb4f7bcaceec5fd3eac54
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-22 16:14:31 +1100

    HELP command done

--- 12c - Spike - Command Pattern/Zorkish/Command.cpp
+++ 12c - Spike - Command Pattern/Zorkish/Command.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "Command.h"
 #include "Adventure.h"
 
@@ -20,28 +22,68 @@ pair<bool, string> GoCommand::execute( vector<string> args,Adventure& adventure)
 	return pair<bool, string>(badInput, resultString);
 }
 
-pair<bool, string> HelpCommand::execute(vector<string> args, Adventure& adventure) {
-	return pair<bool, string>(true, "Help not implemented yet");
+string GoCommand::syntax() {
+	return "GO_[id] || sets current position to location with id [id]";
 }
 
+//HELP command
+pair<bool, string> HelpCommand::execute(vector<string> args, Adventure& adventure) {
+	cout << "========HELP==========" << endl;
+	cout << "_ means a space" << endl;
+	for (const auto& cmd : adventure.cmdManager.commands) {
+		cout << cmd.first << " : " << cmd.second->syntax() << endl;
+	}
+	cout << "======================" << endl;
+	return pair<bool, string>(false, "HELP Done");
+}
+
+string HelpCommand::syntax() {
+	return "HELP || displays syntax of all current commands";
+}
+
+//INVENTORY command
 pair<bool, string> InventoryCommand::execute(vector<string> args, Adventure& adventure) {
 	return pair<bool, string>(true, "Inventory not implemented yet");
 }
 
+string InventoryCommand::syntax() {
+	return "INVENTORY || displays players current inventory";
+}
+
+//LOOK command
 pair<bool, string> LookCommand::execute(vector<string> args, Adventure& adventure) {
 	return pair<bool, string>(true, "Look not implemented yet");
 }
 
+string LookCommand::syntax() {
+	return  "LOOK || displays current locations paths and entities || LOOK_AT_[id] || displays entity with [id] description";
+}
+
+//ALIAS command
 pair<bool, string> AliasCommand::execute(vector<string> args, Adventure& adventure) {
 	return pair<bool, string>(true, "Alias not implemented yet");
 }
 
+string AliasCommand::syntax() {
+	return "ALIAS_[cmd1]_TO_[cmd2] || Sets commandName (e.g GO) of [cmd1] to [cmd2]";
+}
+
+//DEBUG command
 pair<bool, string> DebugCommand::execute(vector<string> args, Adventure& adventure) {
 	return pair<bool, string>(true, "Debug not implemented yet");
 }
 
+string DebugCommand::syntax() {
+	return "DEBUG || shows all locations and all of their info";
+}
+
+//QUIT command
 pair<bool, string> QuitCommand::execute(vector<string> args, Adventure& adventure) {
 	adventure.running = false;
 	return pair<bool, string>(false, "QUIT done");
+}
+
+string QuitCommand::syntax() {
+	return "QUIT || Exits program";
 }
 
--- 12c - Spike - Command Pattern/Zorkish/Command.h
+++ 12c - Spike - Command Pattern/Zorkish/Command.h
@@ -8,35 +8,42 @@ class Adventure;
 
 class Command {
 public:
-	string sytnax;
 	virtual ~Command(){}
 	virtual pair<bool, string> execute(vector<string> args, Adventure& adventure) = 0;
+	virtual string syntax() = 0;
 };
 
 class GoCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class HelpCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class InventoryCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class LookCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class AliasCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class DebugCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };
 
 class QuitCommand : public Command {
+	string syntax() override;
 	pair<bool, string> execute(vector<string> args, Adventure& adventure) override;
 };

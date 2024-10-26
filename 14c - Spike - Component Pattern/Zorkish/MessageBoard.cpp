04f083d842bb181f94e237242fdd68dba9bda9f8
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-26 20:33:18 +1100

    slight improvement to message board, get the functions to their planned state

--- 14c - Spike - Component Pattern/Zorkish/MessageBoard.cpp
+++ 14c - Spike - Component Pattern/Zorkish/MessageBoard.cpp
@@ -6,11 +6,18 @@
 using namespace std;
 
 void MessageBoard::addMessage(Message message) {
-
+	messages.push_back(message);
 }
 
 void MessageBoard::doMessages(Adventure& adventure) {
-
+	for (Message message : messages) {
+		if (adventure.graph[adventure.current].contents.count(message.to)) {
+			adventure.graph[adventure.current].contents[message.to].recieveMessage(message);
+		}
+		else if (adventure.player.inventory.count(message.to)) {
+			adventure.player.inventory[message.to].recieveMessage(message);
+		}
+	}
 }
 
 bool MessageBoard::areThereMessages() {

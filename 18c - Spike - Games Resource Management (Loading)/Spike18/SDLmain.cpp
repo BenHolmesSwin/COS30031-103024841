c94f1d2e06f7a3fa83ec2aca14e9edbc5ac35ae3
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-29 15:20:59 +1100

    updated spike 18 and lab 17 to use relative paths and added the sdl libraries to the Github repository

--- 18c - Spike - Games Resource Management (Loading)/Spike18/SDLmain.cpp
+++ 18c - Spike - Games Resource Management (Loading)/Spike18/SDLmain.cpp
@@ -1,7 +1,10 @@
 
 #include "SDLgraphical.h"
+#include "SDLsound.h"
+
 
 int main(int argc, char* args[]) {
+	SDLsound();
 	SDLgraphical();
 	return 0;
 }
---
+++ 18c - Spike - Games Resource Management (Loading)/Spike18/SDLsound.cpp
@@ -0,0 +1,14 @@
+/*This source code copyrighted by Lazy Foo' Productions 2004-2024
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <random>
+
+#include "SDLsound.h"
+
+void SDLsound() {
+
+}

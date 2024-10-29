ea40d9f64b246bfa940d8aa12246a4af9aa5682b
BenHolmesSwin <103024841@student.swin.edu.au>
2024-10-29 17:50:38 +1100

    toggle works, adjusting sprite limits to look better

--- 18c - Spike - Games Resource Management (Loading)/Spike18/SDLgraphical.cpp
+++ 18c - Spike - Games Resource Management (Loading)/Spike18/SDLgraphical.cpp
@@ -10,7 +10,9 @@ const int SCREEN_WIDTH = 600;
 const int SCREEN_HEIGHT = 800;
 
 //window and background image
+bool backgroundRend = true;
 SDL_Surface* gBackground = NULL;
+SDL_Texture* gBackgroundTexture;
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 
@@ -22,6 +24,19 @@ SDL_Rect gSpriteClips[3];
 SDL_Texture* gSpriteSheetTexture;
 int textHeight = 0;
 int textWidth = 0;
+
+//sprite1 render values
+bool sprite1Rend = true;
+int sprite1Rendx = 0;
+int sprite1Rendy = 0;
+//sprite1 render values
+bool sprite2Rend = true;
+int sprite2Rendx;
+int sprite2Rendy;
+//sprite3 render values
+bool sprite3Rend = true;
+int sprite3Rendx;
+int sprite3Rendy;
 
 void SDLgraphical()
 {
@@ -35,7 +50,6 @@ void SDLgraphical()
         else {
             SDL_Event e;
             bool quit = false;
-            SDL_BlitSurface(gBackground, NULL, gScreenSurface, NULL);
             SDL_UpdateWindowSurface(gWindow);
             while (quit == false) {
                 while (SDL_PollEvent(&e) != 0) {
@@ -43,33 +57,39 @@ void SDLgraphical()
                         quit = true;
                     }
                     else if (e.type == SDL_KEYDOWN) {
-                        int randx;
-                        int randy;
+                        
+                        std::string toggle = "OFF";
                         switch (e.key.keysym.sym)
                         {
                         case SDLK_1://tile 1 toggle
-                            randx = rand() % (SCREEN_WIDTH - gSpriteClips[0].w);
-                            randy = rand() % (SCREEN_HEIGHT - gSpriteClips[0].h);
-                            renderSprite(randx, randy, &gSpriteClips[0]);
-                            printf("Tile 1 display ON at location (%i,%i)\n", randx, randy);
+                            sprite1Rend = !sprite1Rend;
                             break;
                         case SDLK_2://tile 2 toggle
-                            randx = rand() % (SCREEN_WIDTH - gSpriteClips[1].w);
-                            randy = rand() % (SCREEN_HEIGHT - gSpriteClips[1].h);
-                            renderSprite(randx, randy, &gSpriteClips[1]);
-                            printf("Tile 2 display ON at location (%i,%i)\n", randx, randy);
+                            sprite2Rend = !sprite2Rend;
                             break;
-                        case SDLK_3://tile 3 random
-                            randx = rand() % (SCREEN_WIDTH - gSpriteClips[2].w);
-                            randy = rand() % (SCREEN_HEIGHT - gSpriteClips[2].h);
-                            renderSprite(randx, randy, &gSpriteClips[2]);
-                            printf("Tile 3 display ON at location (%i,%i)\n", randx, randy);
+                        case SDLK_3://tile 3 toggle
+                            sprite3Rend = !sprite3Rend;
                             break;
                         case SDLK_0://background toggle
+                            backgroundRend = !backgroundRend;
                             break;
                         }
-                        SDL_RenderPresent(gRenderer);
+                        
                     }
+                    SDL_RenderClear(gRenderer);
+                    if (backgroundRend) {
+                        SDL_RenderCopy(gRenderer,gBackgroundTexture,NULL,NULL);
+                    }
+                    if (sprite1Rend) {
+                        renderSprite(sprite1Rendx, sprite1Rendy, &gSpriteClips[0]);
+                    }
+                    if (sprite2Rend) {
+                        renderSprite(sprite2Rendx, sprite2Rendy, &gSpriteClips[1]);
+                    }
+                    if (sprite3Rend) {
+                        renderSprite(sprite3Rendx, sprite3Rendy, &gSpriteClips[2]);
+                    }
+                    SDL_RenderPresent(gRenderer);
                 }
             }
         }
@@ -112,6 +132,7 @@ bool graphLoadMedia()
     bool success = true;
 
     gBackground = SDL_LoadBMP("resourcesGraphical/Background.bmp");
+    gBackgroundTexture = SDL_CreateTextureFromSurface(gRenderer, gBackground);
     if (gBackground == NULL) {
         printf("Failed to load Background!\n");
         success = false;
@@ -126,23 +147,33 @@ bool graphLoadMedia()
         textHeight = loadedSurface->h;
         textWidth = loadedSurface->w;
 
-        //Set leftmost sprite
+        // initialize random seed:
+        srand(time(NULL));
+
+        //Set leftmost sprite (circle)
         gSpriteClips[0].x = 0;
         gSpriteClips[0].y = 0;
         gSpriteClips[0].w = 70;
         gSpriteClips[0].h = 75;
+        sprite1Rendx = rand() % (SCREEN_WIDTH - gSpriteClips[0].w);
+        sprite1Rendy = rand() % (SCREEN_HEIGHT - gSpriteClips[0].h);
 
-        //Set top right sprite
+
+        //Set middle sprite (triangle)
         gSpriteClips[1].x = 70;
         gSpriteClips[1].y = 0;
         gSpriteClips[1].w = 60;
         gSpriteClips[1].h = 75;
+        sprite2Rendx = rand() % (SCREEN_WIDTH - gSpriteClips[1].w);
+        sprite2Rendy = rand() % (SCREEN_HEIGHT - gSpriteClips[1].h);
 
-        //Set bottom left sprite
-        gSpriteClips[2].x = 130;
-        gSpriteClips[2].y = 0;
-        gSpriteClips[2].w = 70;
-        gSpriteClips[2].h = 75;
+        //Set rightmost sprite (square)
+        gSpriteClips[2].x = 131;
+        gSpriteClips[2].y = 6;
+        gSpriteClips[2].w = 55;
+        gSpriteClips[2].h = 56;
+        sprite3Rendx = rand() % (SCREEN_WIDTH - gSpriteClips[2].w);
+        sprite3Rendy = rand() % (SCREEN_HEIGHT - gSpriteClips[2].h);
     }
 
     //Get rid of old loaded surface
